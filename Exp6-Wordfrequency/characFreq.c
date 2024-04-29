7e0745e78eb92992b1932acef8388d74fd749e4d
Your Name <you@example.com>
2024-04-29 07:14:51 +0530

    Refactor character frequency counting logic and improve word splitting in characFreq.c

--- Exp6-Wordfrequency/characFreq.c
+++ Exp6-Wordfrequency/characFreq.c
@@ -1,31 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
     char str[100];
-    printf("Enter a string: ");
+    printf("Enter a sentence: ");
     fgets(str, sizeof(str), stdin);
-    int length = strlen(str) - 1; // Exclude the newline character
-    int fr[length];
-    int visited = -1;
-    for (int i = 0; i < length; i++) {
-        int count = 1;
-        for (int j = i + 1; j < length; j++) {
-            if (str[i] == str[j]) {
-                count++;
-                fr[j] = visited;
-            } }
-        if (fr[i] != visited)
-            fr[i] = count;
+
+    // Split the input into words
+    char* word = strtok(str, " \n");
+    int count = 0;
+    char words[100][100];
+
+    while (word != NULL) {
+        strcpy(words[count], word);
+        count++;
+        word = strtok(NULL, " \n");
     }
+
+    // Count the frequency of each word
+    int frequency[count];
+    int visited = -1;
+
+    for (int i = 0; i < count; i++) {
+        int wordCount = 1;
+        for (int j = i + 1; j < count; j++) {
+            if (strcmp(words[i], words[j]) == 0) {
+                wordCount++;
+                frequency[j] = visited;
+            }
+        }
+        if (frequency[i] != visited)
+            frequency[i] = wordCount;
+    }
+
+    // Print the word and its frequency
     printf("---------------------\n");
-    printf(" Character | Frequency\n");
+    printf("   Word    | Frequency\n");
     printf("---------------------\n");
-    for (int i = 0; i < length; i++) {
-        if (fr[i] != visited) {
-            printf("    %c", str[i]);
-            printf("    |  ");
-            printf("  %d\n", fr[i]);
-        }}
+
+    for (int i = 0; i < count; i++) {
+        if (frequency[i] != visited) {
+            printf("%-10s | %d\n", words[i], frequency[i]);
+        }
+    }
+
     printf("---------------------\n");
 
     return 0;
